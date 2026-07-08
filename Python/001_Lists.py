a127090906db8b688ffcec0ee485e7bccdf45cd5
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-07-08 22:50:15 +0530

    Add files via upload

---
+++ Python/001_Lists.py
@@ -0,0 +1,7 @@
+numbers = [10, 20, 30, 40]
+
+numbers.append(50)
+
+print("Numbers:", numbers)
+print("First:", numbers[0])
+print("Last:", numbers[-1])
---
+++ Python/002_Tuples.py
@@ -0,0 +1,6 @@
+fruits = ("Apple", "Banana", "Orange")
+
+print("Tuple:", fruits)
+print("First Fruit:", fruits[0])
+print("Second Fruit:", fruits[1])
+print("Length:", len(fruits))
