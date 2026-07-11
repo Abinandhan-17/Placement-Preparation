06932e6c705c5da9a07ef583ae166f02a97b7ee6
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-07-11 21:44:35 +0530

    Added DSA Solutions

---
+++ DSA/MoveZeroes.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        while (left < nums.length) {
+            nums[left] = 0;
+            left++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 3, 12};
+        moveZeroes(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
