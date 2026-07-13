6bb8d1044fa9d6e83d4b9a0435943ca1e5a43f78
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-07-13 22:02:43 +0530

    Added SQL Queries

---
+++ SQL/007_DELETE.sql
@@ -0,0 +1,14 @@
+DELETE FROM Employees
+WHERE EmployeeID = 1;
+
+DELETE FROM Employees
+WHERE Salary < 50000;
+
+DELETE FROM Employees
+WHERE Department = 'Sales';
+
+DELETE FROM Employees
+WHERE HireDate < '2020-01-01';
+
+DELETE FROM Employees
+WHERE LastName = 'Smith';
