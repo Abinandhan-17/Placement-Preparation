60e5f6e9bdca2dd8a6767a3592c838a7c329196f
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-07-22 22:07:04 +0530

    Added DSA Solutions

---
+++ DSA/DistributeCandies.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class DistributeCandies {
+
+    public static int distributeCandies(int[] candyType) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int candy : candyType) {
+            set.add(candy);
+        }
+
+        return Math.min(set.size(), candyType.length / 2);
+    }
+
+    public static void main(String[] args) {
+
+        int[] candyType = {1, 1, 2, 2, 3, 3};
+
+        System.out.println(distributeCandies(candyType));
+    }
+}
