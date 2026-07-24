7362811a1df8437bce044ba31ff1a32aaa426ac3
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-07-24 21:40:54 +0530

    Added DSA Solutions

---
+++ DSA/LongestContinuousIncreasingSubsequence.java
@@ -0,0 +1,32 @@
+public class LongestContinuousIncreasingSubsequence {
+
+    public static int findLengthOfLCIS(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int max = 1;
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] > nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 3, 5, 4, 7};
+
+        System.out.println(findLengthOfLCIS(nums));
+    }
+}
