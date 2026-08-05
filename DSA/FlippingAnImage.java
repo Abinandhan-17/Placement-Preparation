9450df93513273af854d95eaea636e6ede7d27ae
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-08-05 22:19:26 +0530

    Added DSA Solutions

---
+++ DSA/FlippingAnImage.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+public class FlippingAnImage {
+
+    public static int[][] flipAndInvertImage(int[][] image) {
+
+        int n = image.length;
+
+        for (int i = 0; i < n; i++) {
+
+            int left = 0;
+            int right = n - 1;
+
+            while (left <= right) {
+
+                int temp = image[i][left] ^ 1;
+                image[i][left] = image[i][right] ^ 1;
+                image[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+
+        return image;
+    }
+
+    public static void main(String[] args) {
+
+        int[][] image = {
+                {1, 1, 0},
+                {1, 0, 1},
+                {0, 0, 0}
+        };
+
+        int[][] result = flipAndInvertImage(image);
+
+        for (int[] row : result) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
