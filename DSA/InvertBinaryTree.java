740ec903ef1d09268deb98a13f3c33a8deb04e44
ABINANDHAN LOGANATHAN <abinandhanlgrade9a@gmail.com>
2026-08-28 21:17:36 +0530

    Added DSA Solutions

---
+++ DSA/InvertBinaryTree.java
@@ -0,0 +1,75 @@
+public class InvertBinaryTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static TreeNode invertTree(TreeNode root) {
+
+        // Base case
+        if (root == null) {
+            return null;
+        }
+
+        // Swap left and right
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        // Invert left subtree
+        invertTree(root.left);
+
+        // Invert right subtree
+        invertTree(root.right);
+
+        return root;
+    }
+
+    // Inorder traversal to display the tree
+    public static void inorder(TreeNode root) {
+
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+
+        /*
+                 4
+                / \
+               2   7
+              / \ / \
+             1  3 6  9
+        */
+
+        TreeNode root = new TreeNode(4);
+
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(7);
+
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(3);
+
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(9);
+
+        System.out.println("Before Inversion:");
+        inorder(root);
+
+        invertTree(root);
+
+        System.out.println("\nAfter Inversion:");
+        inorder(root);
+    }
+}
